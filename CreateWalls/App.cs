95894cbb4fbcb8a8b86390be8fd7fe15fe159cdc
kraftwerk15 <micah.gray.03@gmail.com>
2020-07-15 13:23:08 -0500

    reconfigure for DesignAutomation

--- CreateWalls/App.cs
+++ CreateWalls/App.cs
@@ -5,7 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Autodesk.Revit.DB;
-using Autodesk.Revit.UI;
+using Autodesk.Revit.ApplicationServices;
+using DesignAutomationFramework;
 using System.Windows.Media.Imaging;
 
 namespace CreateWalls
@@ -13,39 +14,69 @@ namespace CreateWalls
     [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
     [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
 
-    class App : IExternalApplication
+    public class App : IExternalDBApplication
     {
         public static string ExecutingAssemblyPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
 
-        public Result OnStartup(UIControlledApplication application)
+        public ExternalDBApplicationResult OnStartup(ControlledApplication application)
         {
             // Crear Tab 1 Dynoscript
-            string tabName = "REACT-BIM";
-            application.CreateRibbonTab(tabName);
+            // string tabName = "REACT-BIM";
+            //application.CreateRibbonTab(tabName);
 
             // Crear Panel 1
-            RibbonPanel panel11 = application.CreateRibbonPanel(tabName, "Mini House Creation");
+            //RibbonPanel panel11 = application.CreateRibbonPanel(tabName, "Mini House Creation");
 
 
             // agregar un boton
-            PushButton button11 = panel11.AddItem(new PushButtonData("CreateWallsButton", "CREATE", ExecutingAssemblyPath, "CreateWalls.ThisApplication")) as PushButton;
+            //PushButton button11 = panel11.AddItem(new PushButtonData("CreateWallsButton", "CREATE", ExecutingAssemblyPath, "CreateWalls.ThisApplication")) as PushButton;
 
 
             // agregar la imagen al button1
             //button11.LargeImage = new BitmapImage(new Uri("pack://application:,,,/CreateWalls;component/Resources/split-(2).png"));
 
-            button11.ToolTip = "..";
-            button11.LongDescription = "..";
-
-            return Result.Succeeded;
+            //button11.ToolTip = "..";
+            //button11.LongDescription = "..";
+            DesignAutomationBridge.DesignAutomationReadyEvent += HandleDesignAutomationReadyEvent;
+            return ExternalDBApplicationResult.Succeeded;
         }
 
-        public Result OnShutdown(UIControlledApplication application)
+        public ExternalDBApplicationResult OnShutdown(ControlledApplication application)
         {
-
-            return Result.Succeeded;
+            return ExternalDBApplicationResult.Succeeded;
         }
 
+        public void HandleDesignAutomationReadyEvent(object sender, DesignAutomationReadyEventArgs e)
+        {
+            e.Succeeded = true;
+            SketchItFunc(e.DesignAutomationData);
+        }
 
+        private static void SketchItFunc(DesignAutomationData data)
+        {
+            if (data == null)
+                throw new InvalidDataException(nameof(data));
+
+            Application rvtApp = data.RevitApp;
+            if (rvtApp == null)
+                throw new InvalidDataException(nameof(rvtApp));
+
+            Document newDoc = rvtApp.NewProjectDocument(UnitSystem.Imperial);
+            if (newDoc == null)
+                throw new InvalidOperationException("Could not create new document.");
+            string filePath = "sketchIt.rvt";
+
+            string filepathJson = "SketchItInput.json";
+            //SketchItParams jsonDeserialized = SketchItParams.Parse(filepathJson);
+
+            //CreateWalls(jsonDeserialized, newDoc);
+
+            //CreateFloors(jsonDeserialized, newDoc);
+
+            Construct c = new Construct();
+            c.place_WallsDoorsWindows(newDoc);
+
+            newDoc.SaveAs(filePath);
+        }
     }
 }
